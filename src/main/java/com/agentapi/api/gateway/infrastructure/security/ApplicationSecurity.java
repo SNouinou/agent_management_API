1c4de6a92b83248a3f73a7771feb321ab13a015b
NOUINOU Saad <nouinou.saad@gmail.com>
2022-02-10 19:50:01 +0100

    feature_1 [feature][config] add application security module. set runtime port 9090. and set security off for "/api/users/generate"

---
+++ src/main/java/com/agentapi/api/gateway/infrastructure/security/ApplicationSecurity.java
@@ -0,0 +1,20 @@
+package com.agentapi.api.gateway.infrastructure.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+	
+	  @Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http
+	      .authorizeRequests()
+	        .antMatchers("/api/users/generate").permitAll()
+	        .anyRequest().authenticated();
+	  }
+
+}
