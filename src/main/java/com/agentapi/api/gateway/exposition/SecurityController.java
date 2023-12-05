0a8c5f12104e7eac5463dafd6ae16469ab4fecdb
NOUINOU Saad <nouinou.saad@gmail.com>
2023-12-05 22:16:32 +0100

    feature_7 [fix][chore] improve exception handling of login endpoint. correct the exception type for UsersJpaRepoImpl. fix redundant bdd search for user by SecurityService (remove user fetching and let only authencationManager to look for user). use the appropriate UserRole Class in JwtTokenProviderService.

--- src/main/java/com/agentapi/api/gateway/exposition/SecurityController.java
+++ src/main/java/com/agentapi/api/gateway/exposition/SecurityController.java
@@ -4,10 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.agentapi.api.gateway.infrastructure.service.LoginRequest;
 import com.agentapi.api.gateway.infrastructure.service.LoginResponse;
@@ -23,14 +27,17 @@ public class SecurityController {
 	private SecurityService securityService;
 	
     @PostMapping(value = "")
-    public ResponseEntity<LoginResponse> login(HttpServletRequest requestHeader, @RequestBody LoginRequest request) throws RuntimeException {
-
+    public ResponseEntity<LoginResponse> login(HttpServletRequest requestHeader, @RequestBody LoginRequest request) {
+    	try {
         LoginResponse loginResponse = securityService.login(request.getUsername(), request.getPassword());
-        if(loginResponse == null){
-            throw new RuntimeException("Login failed.");
-        }else{
             return new ResponseEntity<>(loginResponse, HttpStatus.OK);
-        }
+    	} catch(BadCredentialsException e) {
+    		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found.");
+    	} catch(DisabledException e) {
+    		throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User does not have access.");
+    	} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+		}
     }
 
 }
